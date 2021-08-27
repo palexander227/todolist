59f70da9ed8bfa703dcc83cf84e32cb31f9ec447
Peter Alexander <renormalize@gmail.com>
2021-08-26 20:30:17 -0700

    Updates

---
+++ precache-manifest.8a6f0e4528cfd5e5258f0cf8e6bc8f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5425942ce176d6533f8025fa2e09b47",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9312dda80161ea1024df",
+    "url": "/todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cf6b9d126e0df03d2b0",
+    "url": "/todolist/static/js/2.152314b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.152314b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9312dda80161ea1024df",
+    "url": "/todolist/static/js/main.10100296.chunk.js"
+  },
+  {
+    "revision": "650ffcdc0545585823ad",
+    "url": "/todolist/static/js/runtime-main.25277596.js"
+  }
+]);
